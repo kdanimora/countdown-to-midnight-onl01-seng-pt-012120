546ba1bc8b2cb519018fcdc17df5807c6438d75f
Karen Daniela <kdaniimora@gmail.com>
2020-01-27 20:37:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,9 @@ def countdown_with_sleep(seconds_to_midnight)
     while seconds_to_midnight < 0 
    puts "#{seconds_to_midnight} Second(s)"
    sleep(1)
-   
+   seconds_to_midnight -+1
+ end
+ "Happy New Year!!"
 
 end
 
